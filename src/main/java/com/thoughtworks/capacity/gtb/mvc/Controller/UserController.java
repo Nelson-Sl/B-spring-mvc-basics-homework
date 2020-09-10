b0f328eb2bb393314dca765aee7b74e34cfecdd5
Shen Liao <shen.liao@thoughtworks.com>
2020-09-10 17:47:18 +0800

    [Nelson Liao] feat: finish login api with validation

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/Controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capacity.gtb.mvc.Controller;
 
+import com.thoughtworks.capacity.gtb.mvc.Exception.UserNotFoundException;
 import com.thoughtworks.capacity.gtb.mvc.Service.UserService;
 import com.thoughtworks.capacity.gtb.mvc.domain.User;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @RestController
 public class UserController {
@@ -23,8 +27,12 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public User userLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public User userLogin(@RequestParam("username") @Size(min=3, max=10) @Pattern(regexp = "[_a-zA-Z0-9]+") String username,
+                          @RequestParam("password") @Size(min=5, max=12) String password) {
         User loginUser = this.userService.loginUser(username, password);
+        if(loginUser == null) {
+            throw new UserNotFoundException("This user hasn't been registered yet. Please register");
+        }
         return loginUser;
     }
 }
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/Exception/GlobalExceptionHandler.java
@@ -6,12 +6,27 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResult> handler(UserNotFoundException ex) {
+        String message = ex.getMessage();
+        ErrorResult errorResult = new ErrorResult(message);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResult> handler (MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult().getFieldError().getDefaultMessage();
+        ErrorResult errorResult = new ErrorResult(message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResult> handler (ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations().stream().findFirst().toString();
         ErrorResult errorResult = new ErrorResult(message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
     }
